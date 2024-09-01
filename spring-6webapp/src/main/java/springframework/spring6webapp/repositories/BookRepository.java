e74edb0f139745976dcd98f43ba678403f917785
RosenMitrov <rdatabase.m@gmail.com>
2024-09-01 15:35:13 +0300

    SPRING-0005 Adding package for repositories.

---
+++ spring-6webapp/src/main/java/springframework/spring6webapp/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package springframework.spring6webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import springframework.spring6webapp.domain.Book;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
