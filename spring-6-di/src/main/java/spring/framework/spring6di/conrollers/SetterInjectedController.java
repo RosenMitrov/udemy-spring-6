d5adf91b25f193f4907d98a714b9bf720eea1c5b
RosenMitrov <rdatabase.m@gmail.com>
2024-09-11 22:03:21 +0300

    SPRING-0017 Adding dependency injection using Spring Framework.

--- spring-6-di/src/main/java/spring/framework/spring6di/conrollers/SetterInjectedController.java
+++ spring-6-di/src/main/java/spring/framework/spring6di/conrollers/SetterInjectedController.java
@@ -1,11 +1,16 @@
 package spring.framework.spring6di.conrollers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import spring.framework.spring6di.services.GreetingService;
 
+@Controller
 public class SetterInjectedController {
 
+    //Less recommended way - setter injection.
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
--- spring-6-di/src/test/java/spring/framework/spring6di/conrollers/ConstructorInjectedControllerTest.java
+++ spring-6-di/src/test/java/spring/framework/spring6di/conrollers/ConstructorInjectedControllerTest.java
@@ -1,18 +1,18 @@
 package spring.framework.spring6di.conrollers;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import spring.framework.spring6di.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class ConstructorInjectedControllerTest {
 
+    @Autowired
     ConstructorInjectedController constructorInjectedController;
-    @BeforeEach
-    void setUp() {
-        this.constructorInjectedController = new ConstructorInjectedController(new GreetingServiceImpl());
-    }
+//    @BeforeEach
+//    void setUp() {
+//        this.constructorInjectedController = new ConstructorInjectedController(new GreetingServiceImpl());
+//    }
 
     @Test
     void sayHello() {
